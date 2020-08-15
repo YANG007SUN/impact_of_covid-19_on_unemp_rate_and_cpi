9d7dd2315687c62621206ce8b86e1cd5d20deea8
obfrap <masterofdestruction@gmail.com>
2020-08-14 18:40:45 -0700

    Updates included corrected plots.  Line chart is plotted on three separate plots but has mutli axis code commented out in case want to use in future.

--- Peter/static/js/lineChart.js
+++ Peter/static/js/lineChart.js
@@ -1,4 +1,4 @@
-var testState = "Nevada"
+var testState = "Washington"
 // Do not actually need test date variable
 var testDate = "2020-4"
 
@@ -6,9 +6,9 @@ var testDate = "2020-4"
 //Create function to build plot
 
 d3.json("mapdata2.json").then(function(covidData) {
-  //console.log(covidData.features);
+  
   var covidFeatures = covidData.features;
-  //console.log(covidFeatures);
+  
   covidFeatures.forEach(function(d) {
     var names = d.properties.name;
     var totalCases = d.properties.total_cases;
@@ -19,11 +19,7 @@ d3.json("mapdata2.json").then(function(covidData) {
     var stateCases =[];
     var stateDates = [];
     var stateUnempt = [];
-    // console.log(names);
-    // console.log(totalCases);
-    // console.log(totalDeaths);
-    // console.log(unempt);
-    // console.log(date);
+    
 
     if (names === testState)  {
       var stateDates = date;
@@ -38,8 +34,7 @@ d3.json("mapdata2.json").then(function(covidData) {
       var trace1 = {
         x: stateDates,
         y: stateCases,
-        name: "Covid Cases",
-        yaxis: 'y1',
+        name: "Covid Cases",        
         type: "scatter",          
           
       };
@@ -48,6 +43,7 @@ d3.json("mapdata2.json").then(function(covidData) {
         x: stateDates,
         y: stateDeaths,
         name: "Covid Deaths",
+        xaxis: 'x2',
         yaxis: 'y2',
         type: "scatter", 
       };
@@ -56,6 +52,7 @@ d3.json("mapdata2.json").then(function(covidData) {
         x: stateDates,
         y: stateUnempt,
         name: "Unemployement Rate (%)",
+        xaxis: 'x3',
         yaxis: 'y3',
         type: "scatter",
       };
@@ -63,42 +60,43 @@ d3.json("mapdata2.json").then(function(covidData) {
       var data = [trace1, trace2, trace3];
 
       var layout = {
-        title: 'Covid Cases, Deaths and Unemployement Rate',
-        autosize: false,
-        width: 1500,
-        height: 1500,
-        xaxis: {
-          range: [0,1],
-        },       
-        yaxis: {
-          title: 'Covid Cases',
-          titlefont: {color: '#1f77b4'},
-          tickfont: {color: '#1f77b4'},
-          
-          side: 'left',         
-        },
-        
-        yaxis2: {
-          title: 'Covid Deaths',
-          titlefont: {color: '#ff7f0e'},
-          tickfont: {color: '#ff7f0e'},
-          anchor: 'x',
-          overlay: "y",        
-          side: 'right',
-          // position: 0.15,        
-        },
-        
-        yaxis3: {
-          title: 'Unemployement Rate (%)',
-          titlefont: {color: 'green'},
-          tickfont: {color: 'green'},
-          //anchor: 'x',
-          overlay: "n",
-          side: 'left',
-          position: 0.15
-          
-        }
-      }; 
+        grid: {
+            rows: 3,
+            columns: 1,
+            pattern: 'independent',
+            roworder: 'top to bottom'}
+        };
+      // Commented out, but can be used if want to use multi y axis on same plot.
+      // var layout = {
+      //   title: 'Covid Cases, Deaths and Unemployement Rate',
+      //   xaxis: {
+      //     anchor: 'y1',
+      //   },
+      //   yaxis: {
+      //     title: 'Covid Cases',
+      //     titlefont: {color: '#1f77b4'},
+      //     tickfont: {color: '#1f77b4'},
+      //     anchor: 'x1'         
+      //   },
+      //   xaxis2: {
+      //     anchor: 'y2',
+      //   },
+      //   yaxis2: {
+      //     title: 'Covid Deaths',
+      //     titlefont: {color: '#ff7f0e'},
+      //     tickfont: {color: '#ff7f0e'},
+      //     anchor: 'x2',         
+      //   },
+      //   xaxis3: {
+      //     anchor: 'y3'
+      //   },
+      //   yaxis3: {
+      //     title: 'Unemployement Rate (%)',
+      //     titlefont: {color: 'green'},
+      //     tickfont: {color: 'green'},
+      //     anchor: 'x3',      
+      //   }
+      // }; 
       
 
       Plotly.newPlot("plot", data, layout);
